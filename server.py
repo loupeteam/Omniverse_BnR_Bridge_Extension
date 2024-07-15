bffc8c6a4e1eb2dceb4e20fa815298383dd4f9e1
Shane Reetz <shane@loupe.team>
2024-07-15 15:11:05 -0700

    In the mock server, respond to varaibles even if they don't exist on the PLC (useful for now, likely will change)

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import websockets
 import json
 
 fake_plc_data = {"LuxProg:counter": 0}
+INITIAL_VALUE_NEW_READ_VAR = 0
 
 async def echo(websocket, path):
     async for message in websocket:
@@ -18,8 +19,12 @@ async def echo(websocket, path):
         if 'type' in message_dict.keys():
             if message_dict['type'] == "read":
                 for plc_var in message_dict["data"]:
-                    if plc_var in fake_plc_data.keys():
-                        response["data"][plc_var] = fake_plc_data[plc_var]
+                    # If variable doesn't already exist, add it to dictionary
+                    # Pretend it exists on the PLC
+                    if plc_var not in fake_plc_data.keys():
+                        fake_plc_data[plc_var] = INITIAL_VALUE_NEW_READ_VAR
+
+                    response["data"][plc_var] = fake_plc_data[plc_var]
                 
                 await websocket.send(json.dumps(response))
 
