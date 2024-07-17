1ee1101cea59923db61e6be291b71e5cf276ab5b
Shane Reetz <shane@loupe.team>
2024-07-16 18:16:27 -0700

    Update tests to be more useful

--- exts/loupe.simulation.br_bridge/loupe/simulation/br_bridge/tests/tests.py
+++ exts/loupe.simulation.br_bridge/loupe/simulation/br_bridge/tests/tests.py
@@ -7,77 +7,40 @@ passes = 0
 fails = 0
 
 import omni.kit.test
-import loupe.simulation.br_bridge
+from loupe.simulation.br_bridge.websockets_driver import WebsocketsDriver
 
 class TestUtils(omni.kit.test.AsyncTestCase):
 
     # Run before every test
     async def setUp(self):
-        print('setUp')
+        self.driver = WebsocketsDriver(ip='127.0.0.1', port=8000)
     
     # Run after every test
     async def tearDown(self):
-        print('tearDown')
+        pass
 
-    def test_basic(self):
-        print('test basic running')
-        self.assertTrue(True)
+    def test_variable_name_parsing_global(self) -> None:
 
-    def test_basi2c(self):
-        print('test basic running')
-        self.assertTrue(True)
+        value = 30
+        input = "gBool"
+        correct_output = {"gBool": value}
+        actual_output = self.driver._parse_name(name_dict={}, name=input, value=value)
+        self.assertEqual(actual_output, correct_output)
 
-    # def test_variable_name_parsing_global(self) -> None:
+    def test_variable_name_parsing_simple_struct(self) -> None:
 
-    #     driver = WebsocketsDriver(ip='127.0.0.1', port=8000)
+        value = 30
+        input = "Program:struct.var"
+        correct_output = {"Program": {"struct": {"var": value}}}
+        actual_output = self.driver._parse_name(name_dict={}, name=input, value=value)
+        self.assertEqual(actual_output, correct_output)
 
-    #     value = 30
-    #     input = "Program:struct.var"
-    #     correct_output = {"Program": {"struct": {"var": value}}}
-    #     actual_output = driver._parse_name(name_dict={}, name=input, value=value)
-    #     self.assertEqual(actual_output, correct_output)
+    def test_variable_name_parsing_simple_array(self) -> None:
 
-#     def test_variable_structured(self) -> None:
-
-
-# # first element is input, second is expected output
-# br_vars = [
-    
-#     ["Global", {"Global": value}],
-#     ["Program:struct.array[30]]", {"Program": {"struct": {"array": [value] * array_size}}}],
-# ]
-
-# beckhoff_vars = [
-#     ["Program.struct.var", {"Program": {"struct": {"var": value}}}],
-#     ["Global", {"Global": value}],
-#     ["Program.struct.array[30]]", {"Program": {"struct": {"array": [value] * array_size}}}],
-# ] 
-#       #  ["Global", } "Global.struct.var", "Program:struct.array[30]", "Program:array[0]", "Program:struct.array[20].struct", "gArray[10]"]
-
-# def perform_test(expected, result):
-#     if expected == result:
-#         print("PASS")
-#         global passes
-#         passes += 1
-#     else:
-#         print("FAILLLLLLLL")
-#         global fails
-#         fails += 1
-
-#     print('for var string: ' + str(expected) + '\n' + str(result))
-#     print('\n\n\n')
-
-# def run_all_tests():
-#     for test_condition in br_vars:
-#         result = driver._parse_name(name_dict={}, name=test_condition[0], value=value)
-#         perform_test(expected=test_condition[1], result=result)
-
-#     # TODO test dictionary being built
-#     for test_condition in br_vars:
-#         result = driver._parse_name(name_dict={}, name=test_condition[0], value=value)
-#         perform_test(expected=test_condition[1], result=result)
-
-#     print('Passes:{}\tFails:{}'.format(passes, fails))
-
-# run_all_tests()
+        value = 30
+        array_size = 30
+        input = "Program:array[30]"
+        correct_output = {"Program": {"array" : [value for _ in range(array_size)]}}
+        actual_output = self.driver._parse_name(name_dict={}, name=input, value=value)
+        self.assertEqual(actual_output, correct_output)#
 
