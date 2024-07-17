87a7c61563f6b015c3b58d6331a84a042ec84a1e
Shane Reetz <shane@loupe.team>
2024-07-17 09:59:07 -0700

    Update tests

--- exts/loupe.simulation.br_bridge/loupe/simulation/br_bridge/tests/tests.py
+++ exts/loupe.simulation.br_bridge/loupe/simulation/br_bridge/tests/tests.py
@@ -11,8 +11,11 @@ from loupe.simulation.br_bridge.websockets_driver import WebsocketsDriver
 
 class TestUtils(omni.kit.test.AsyncTestCase):
 
+    # TODO check accumulation of vars, as well as unique vars
+
     # Run before every test
     async def setUp(self):
+        self.name_dict = {}
         self.driver = WebsocketsDriver(ip='127.0.0.1', port=8000)
     
     # Run after every test
@@ -31,7 +34,12 @@ class TestUtils(omni.kit.test.AsyncTestCase):
 
         value = 30
         input = "Program:struct.var"
-        correct_output = {"Program": {"struct": {"var": value}}}
+        correct_output = {
+            "Program": {
+                "struct": {
+                    "var": value}
+                }
+            }
         actual_output = self.driver._parse_name(name_dict={}, name=input, value=value)
         self.assertEqual(actual_output, correct_output)
 
@@ -39,8 +47,24 @@ class TestUtils(omni.kit.test.AsyncTestCase):
 
         value = 30
         array_size = 30
-        input = "Program:array[30]"
-        correct_output = {"Program": {"array" : [value for _ in range(array_size)]}}
+        input = "Program:array[29]"
+        correct_output = {"Program": {"array" : [None] * array_size}}
+        correct_output["Program"]["array"][array_size - 1] = value
+        
         actual_output = self.driver._parse_name(name_dict={}, name=input, value=value)
-        self.assertEqual(actual_output, correct_output)#
+        msg = "\n" + str(correct_output) + "\n" + str(actual_output)
+        self.assertEqual(actual_output, correct_output, msg=msg)
+
+    def test_parse_name_with_array(self):
+        self.driver._parse_name(self.name_dict, "Task:myStruct.myArray[0].myVar", "value")
+        expected = {
+            "Task": {
+                "myStruct": {
+                    "myArray": [
+                        {"myVar": "value"}
+                    ]
+                }
+            }
+        }
+        self.assertEqual(self.name_dict, expected)
 
