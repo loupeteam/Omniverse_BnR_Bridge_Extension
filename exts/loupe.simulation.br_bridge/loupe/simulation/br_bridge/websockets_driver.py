066369835b3f3998c3f322a0054db7193aec5dbf
David Wiens <davidw@loupe.team>
2024-07-18 12:31:53 -0700

    Update _parse_name

--- exts/loupe.simulation.br_bridge/loupe/simulation/br_bridge/websockets_driver.py
+++ exts/loupe.simulation.br_bridge/loupe/simulation/br_bridge/websockets_driver.py
@@ -123,27 +123,38 @@ class WebsocketsDriver():
         return parsed_data
 
     
+    # Ensure that dictionary has a key of list_name, that it's value is a list,
+    #   and that the list is long enough to include the index
+    def _ensure_list_with_index_in_dict(self, list_name, _dict, _index):
+        # Create list if not in dict
+        if list_name not in _dict or not isinstance(_dict[list_name], list):
+            _dict[list_name] = []
+
+        # Extend list if not long enough
+        if _index >= len(_dict[list_name]):
+            _dict[list_name].extend([None] * (_index - len(_dict[list_name]) + 1))
+
+    # Parse name and insert value into correct location in name_dict, creating a location
+    #   in the name_dict if necessary. Returns modified name_dict.
     def _parse_name(self, name_dict, name, value):
 
+        # Split name into parts
+        # Example: "Program:myStruct[3].myVar" -> ["Program", "myStruct[3]", "myVar"]
         name_parts = re.split('[:.]', name)
 
         if len(name_parts) > 1:
-            # Multiple parts to passed-in name (e.g. Program:myStruct[3].myVar has 3 parts)
+            # Multiple parts in passed-in name (e.g. Program:myStruct[3].myVar has 3 parts)
             # From here we want to use recursion to assign a dictionary value (i.e. sub dictionary) to the first part.
 
             first_part_is_array = '[' in name_parts[0]
 
-            ## Ensure corresponding subdictionary exists
+            ## Get pre-existing subdictionary (or create if necessary)
             if first_part_is_array:
                 array_name, index = name_parts[0].split("[")
                 index = int(index[:-1])
                 
-                if array_name not in name_dict or not isinstance(name_dict[array_name], list):
-                    name_dict[array_name] = []
-                
-                # Extend if necessary
-                if index >= len(name_dict[array_name]):
-                    name_dict[array_name].extend([None] * (index - len(name_dict[array_name]) + 1))
+                # Ensure array is in dictionary and is long enough
+                self._ensure_list_with_index_in_dict(array_name, name_dict, index)
 
                 # Ensure array index location has dict-typed value
                 if not isinstance(name_dict[array_name][index], dict):
@@ -161,26 +172,24 @@ class WebsocketsDriver():
             
             # Get subdictionary from using remaining part of path
             sub_name = '.'.join(name_parts[1:])
-            sub_dict = self._parse_name(existing_sub_dict , sub_name, value)
+            sub_dict = self._parse_name(existing_sub_dict, sub_name, value)
             
+            # Assign result of recursive call (subdictionary) to first part
             if first_part_is_array:
                 name_dict[array_name][index] = sub_dict
             else:
                 name_dict[member_name] = sub_dict
         else:
-            # only one part to passed in name
+            # Only one part in passed-in name
+            # Proceed to assign value
 
             if '[' in name_parts[0]:
                 array_name, index = name_parts[0].split("[")
                 index = int(index[:-1])
                 
-                if array_name not in name_dict or not isinstance(name_dict[array_name], list):
-                    name_dict[array_name] = []
-                
-                # Extend if necessary
-                if index >= len(name_dict[array_name]):
-                    name_dict[array_name].extend([None] * (index - len(name_dict[array_name]) + 1))
-                
+                # Ensure array is in dictionary and is long enough
+                self._ensure_list_with_index_in_dict(array_name, name_dict, index)
+
                 name_dict[array_name][index] = value
             else:
                 # Write value (regardless of whether it exists or not)
